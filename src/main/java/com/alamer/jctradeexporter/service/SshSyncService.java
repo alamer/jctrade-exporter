231d31192a02fbe85121ebe44a1233b25e880d38
Ivan Scherbakov <shherbakoviv@gmail.com>
2020-07-13 22:13:26 +0800

    refactor: ssh sync

--- src/main/java/com/alamer/jctradeexporter/service/SshSyncService.java
+++ src/main/java/com/alamer/jctradeexporter/service/SshSyncService.java
@@ -10,7 +10,10 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 
 @Service
@@ -18,11 +21,20 @@ public class SshSyncService {
     Logger logger = LoggerFactory.getLogger(SshSyncService.class);
 
 
-    @Autowired
+    final
     ApplicationConfig config;
 
-    @Autowired
+    final
     RemoteHostConfig remoteHostConfig;
+
+    final
+    XlsPathService xlsPathService;
+
+    public SshSyncService(ApplicationConfig config, RemoteHostConfig remoteHostConfig, XlsPathService xlsPathService) {
+        this.config = config;
+        this.remoteHostConfig = remoteHostConfig;
+        this.xlsPathService = xlsPathService;
+    }
 
     public void syncFolderWithRemote() throws JSchException, SftpException, IOException {
 
@@ -39,21 +51,24 @@ public class SshSyncService {
         File[] fileList = dir.listFiles();
         logger.info(String.format("%d files to go", fileList.length));
         int cnt = 0;
-        for (File f : fileList) {
+        for (File f :fileList) {
             try (FileInputStream fos = new FileInputStream(f)) {
                 channelSftp.put(fos, f.getName(), ChannelSftp.OVERWRITE);
                 logger.info(String.format("%d of %d done", ++cnt, fileList.length));
             }
         }
         logger.info("Sync  XLS files... ");
-        File outputReportDir = new File(config.getOutputReportDir());
-        cnt=0;
-        for (File f : outputReportDir.listFiles()) {
-            try (FileInputStream fos = new FileInputStream(f)) {
-                channelSftp.put(fos, f.getName(), ChannelSftp.OVERWRITE);
-                logger.info(String.format("%d of %d done", ++cnt, fileList.length));
-            }
-        }
+        Path savePath = xlsPathService.getPath(config.getOutputReportDir());
+        logger.info(savePath.toString());
+        Files.walk(savePath).filter(Files::isRegularFile)
+                .forEach(f -> {
+                    logger.info(f.toString());
+                    try (FileInputStream fos = new FileInputStream(f.toFile())) {
+                        channelSftp.put(fos, f.toFile().getName(), ChannelSftp.OVERWRITE);
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
         channelSftp.exit();
         session.disconnect();
 
